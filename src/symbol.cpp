43b70fe3a1f926f5748d0851ec847d579ac06e49
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2025-09-28 23:53:39 -0700

    Added some symbol tests

--- src/symbol.cpp
+++ src/symbol.cpp
@@ -7,11 +7,11 @@ symbol::symbol(const std::string& a_repr, const type& a_type)
 
 bool operator<(const symbol& a_lhs, const symbol& a_rhs)
 {
-    if(a_lhs.m_type < a_rhs.m_type)
+    if(a_lhs.m_repr < a_rhs.m_repr)
         return true;
-    if(a_rhs.m_type < a_lhs.m_type)
+    if(a_rhs.m_repr < a_lhs.m_repr)
         return false;
-    return a_lhs.m_repr < a_rhs.m_repr;
+    return a_lhs.m_type < a_rhs.m_type;
 }
 
 bool operator==(const symbol& a_lhs, const symbol& a_rhs)
@@ -37,6 +37,21 @@ void test_symbol_construction_and_equality()
 
 void test_symbol_comparison()
 {
+    // name comparison
+    {
+        symbol l_symbol("x", type(type::constant("int"), {}));
+        symbol l_symbol2("y", type(type::constant("int"), {}));
+        assert(l_symbol < l_symbol2);
+        assert(!(l_symbol2 < l_symbol));
+    }
+
+    // type comparison
+    {
+        symbol l_symbol("x", type(type::constant("int"), {}));
+        symbol l_symbol2("x", type(type::constant("string"), {}));
+        assert(l_symbol < l_symbol2);
+        assert(!(l_symbol2 < l_symbol));
+    }
 }
 
 void symbol_test_main()

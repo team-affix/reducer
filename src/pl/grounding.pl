d2a18576e04e1a04f987898cb6e958462f5e9515
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2025-10-24 05:07:51 -0700

    Updated grounding and reduction

--- src/pl/grounding.pl
+++ src/pl/grounding.pl
@@ -3,32 +3,32 @@
 
 % define ground_term/2
 % schema: ground_term(!Index, ?>!Term)
-ground_term(Index, Term) :-
+ground_term(Index, Index, Term) :-
     number(Index),
     atom(Term).
-ground_term(Index, Term) :-
+ground_term(Index, NewIndex, Term) :-
     number(Index),
     var(Term),
-    atom_concat(tm, Index, Term).
+    atom_concat(tm, Index, Term),
+    NewIndex is Index + 1.
 
 
 % ground_type/2
 % schema: ground_type(!Index, ?>!Type)
 % Env: environment
 % Type: input type to ground.
-ground_type(Index, Type) :-
+ground_type(Index, Index, Type) :-
     number(Index),
     atom(Type).
 
-ground_type(Index, A@B) :-
+ground_type(Index, NewIndex, A@B) :-
     number(Index),
     nonvar(A), % even though they may be nonground,
     nonvar(B), % we need them to be nonvar for the grounding to work.
-    ground_type(Index, A),
-    NextIndex is Index + 1,
-    ground_type(NextIndex, B).
+    ground_type(Index, Index1, A),
+    ground_type(Index1, NewIndex, B).
 
-ground_type(Index, (A::B)~>C) :-
+ground_type(Index, NewIndex, (A::B)~>C) :-
     number(Index),
     % B should be nonvar at this point, as:
     %     if B was var at any point, and the whole thing is a valid type,
@@ -39,11 +39,9 @@ ground_type(Index, (A::B)~>C) :-
     %     as function types are compound terms.
     nonvar(B),
     % step 1: make sure A is an atom
-    ground_term(Index, A),
+    ground_term(Index, Index1, A),
     % step 2: ground B.
-    ground_type(Index, B),
-    % step 3: compute the next index for naming.
-    NextIndex is Index + 1,
-    % step 4: ground C.
-    ground_type(NextIndex, C).
+    ground_type(Index1, Index2, B),
+    % step 3: ground C.
+    ground_type(Index2, NewIndex, C).
 
--- src/pl/reduction.pl
+++ src/pl/reduction.pl
@@ -1,51 +1,81 @@
 :- ensure_loaded(syntax).
-:- ensure_loaded(application).
+:- ensure_loaded(grounding).
 
+:- table reduce/3.
 
 % define reduce/3
 
-reduce(Defs, A@B, R) :-
-    % A needs reducing
-    reduce(Defs, A, AR),
-    A \= AR,
+% function signature reduction
+reduce(NI, NewNI, Defs, (A::B)~>C, (X::Y)~>Z) :-
+    % if we make it here, then
+    %     this is the only way to reduce the lhs
     !,
-    reduce(Defs, AR@B, R).
-
-reduce(Defs, A@B, R) :-
-    % B needs reducing
-    reduce(Defs, B, BR),
-    B \= BR,
-    !,
-    reduce(Defs, A@BR, R).
-
-reduce(Defs, (A::B)~>C, R) :-
-    % B needs reducing
-    reduce(Defs, B, BR),
-    B \= BR,
-    !,
-    reduce(Defs, (A::BR)~>C, R).
-
-reduce(Defs, (A::B)~>C, R) :-
-    % C needs reducing
-    reduce(Defs, C, CR),
-    C \= CR,
-    !,
-    reduce(Defs, (A::B)~>CR, R).
-
-% Beta reduction: ((X::Type)~>Body) @ Arg reduces to Body[X := Arg]
-reduce(Defs, ((X::Type)~>Body)@Arg, R) :-
-    nonvar(Type),
-    !,
-    % Use apply to perform substitution
-    apply((X::Type)~>Body, [], [Arg], [Type], Substituted),
-    reduce(Defs, Substituted, R).
-
-reduce(Defs, A, BR) :-
-    member([A|B], Defs),
-    reduce(Defs, B, BR),
-    !.
-    
-reduce(Defs, A, A) :-
+    % make sure A is not already defined
     \+ member([A|_], Defs),
-    !.
+    % ground A
+    ground_term(NI, NI1, A),
+    % ground X (must be ground to prevent infinite recursion)
+    ground_term(NI1, NI2, X),
+    % ground B
+    ground_type(NI2, NI3, B),
+    % reduce B
+    reduce(NI3, NI4, Defs, B, Y),
+    % append the new alpha-equivalence mapping to the defs
+    %     and reduce the body.
+    reduce(NI4, NewNI, [[A|X]|Defs], C, Z).
+
+% function definition reduction
+reduce(NI, NewNI, Defs, A~>B, X~>Y) :-
+    % if we make it here, then
+    %     this is the only way to reduce the lhs
+    !,
+    % make sure A is not already defined
+    \+ member([A|_], Defs),
+    % ground A
+    ground_term(NI, NI1, A),
+    % ground X (must be ground to prevent infinite recursion)
+    ground_term(NI1, NI2, X),
+    % append the new alpha-equivalence mapping to the defs
+    %     and reduce the body.
+    reduce(NI2, NewNI, [[A|X]|Defs], B, Y).
+
+reduce(NI, NewNI, Defs, (A~>B)@C, R) :-
+    % if we make it here, then
+    %     this is the only way to reduce the lhs
+    !,
+    % make sure A is not already defined
+    \+ member([A|_], Defs),
+    % ground A
+    ground_term(NI, NI1, A),
+    % append the new alpha-equivalence mapping to the defs
+    %     and reduce the body.
+    reduce(NI1, NewNI, [[A|C]|Defs], B, R).
+
+reduce(NI, NewNI, Defs, A@B, R) :-
+    % reduce A
+    reduce(NI, NI1, Defs, A, AR),
+    % reduce B
+    reduce(NI1, NI2, Defs, B, BR),
+    (
+        % check: is AR a function definition?
+        AR = X~>Y ->
+        % then beta-reduce
+        reduce(NI2, NewNI, [[X|BR]|Defs], Y, R)
+        ;
+        % otherwise, delta reduce
+        member([AR@BR|C], Defs) ->
+        reduce(NI2, NewNI, Defs, C, R)
+        ;
+        % otherwise, return the original term
+
+    ).
+
+% delta reduction if term defined
+reduce(NI, NewNI, Defs, A, BR) :-
+    member([A|B], Defs),
+    !,
+    reduce(NI, NewNI, Defs, B, BR).
+    
+% delta reduction if term undefined
+reduce(NI, NI, _, A, A).
 

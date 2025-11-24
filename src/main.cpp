854422cf9e2ad65638cc3fc4a8ed87949cd775d2
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2025-11-24 01:34:02 -0800

    Uncommented model tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,6 @@
 #include "test_utils.hpp"
 
-extern void scope_test_main();
-extern void func_test_main();
-extern void program_test_main();
 extern void model_test_main();
-extern void reduce_test_main();
 extern void predef_test_main();
 extern void encode_test_main();
 
@@ -15,7 +11,7 @@ void unit_test_main()
     // TEST(scope_test_main);
     // TEST(func_test_main);
     // TEST(program_test_main);
-    // TEST(model_test_main); // Disabled: hangs during execution
+    TEST(model_test_main); // Disabled: hangs during execution
     TEST(predef_test_main);
     TEST(encode_test_main);
     // TEST(reduce_test_main);
